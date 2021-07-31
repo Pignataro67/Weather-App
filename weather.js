f57b6a322d42e0c730ae18ad11aacbfa03fc2e84
Pignataro67 <Pignataro67@gmail.com>
2021-07-31 06:03:02 -0700

    workig on a fetch

--- weather.js
+++ weather.js
@@ -1,8 +1,24 @@
 class Weather {
-  contsructor(city, state) {
-    this.apiKey = 'b41dc92c7c9da50be399459ae11a2e27';
+  contsructor(city, country) {
+    this.apiKey = '0340c197587e2a590809de58f649cc8a';
     this.city = city;
-    this.state = state;
+    this.country = country;
+  }
+
+    // Fetch Weather from API
+  async getWeather() {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.country}&appid=${this.apiKey}`);
+                               // https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={API key}
+    const responseData = await response.json()
+    console.log(responseData);
+
+    return responseData;
+  }
+
+  // Change weather location
+  changeLocation(city, country) {
+    this.city = city
+    this.country = country
   }
 }
 
