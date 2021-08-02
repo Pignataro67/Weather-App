4eab40233ea089809054519fdb58de7e5f7cdf22
Pignataro67 <Pignataro67@gmail.com>
2021-08-02 05:18:57 -0700

    finishing a function

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 // Init weather object
 const weather = new Weather('Boston', 'MA');
 
-weather.getWeather()
+// Get weather on DOM load
+document.addEventListener('DOMContentLoaded', getWeather);
+
+// weather.changeLocation('Miami', 'FL');
+
+function getWeather() {
+  weather.getWeather()
   .then(results => {
-    console.log(results);
+    ui.paint(results);
   })
   .catch(err => console.log(err));
+}
