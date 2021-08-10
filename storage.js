c37a0cf4de5c2b697b18b836e79bbd2f07c9d1ff
Pignataro67 <Pignataro67@gmail.com>
2021-08-10 05:38:30 -0700

    if statement for storage

--- storage.js
+++ storage.js
@@ -7,10 +7,26 @@ class Storage {
   }
 
   get LocationData() {
+    if(localStorage.getItem('city') === null) {
+      this.city = this.defaultCity;
+    } else {
+      this.city = localStorage.getItem('city');
+    }
 
+    if(localStorage.getItem('state') === null) {
+      this.state = thiis.defaultState;
+    } else {
+      this.state = localStorage.getItem('state');
+    }
+
+    return {
+      city: this.city,
+      state: this.state
+    }
   }
 
   setLocation(city, state) {
-    localStorage.setItem();
+    localStorage.setItem('city', city);
+    localStorage.setItem('state', state);
   }
 }
